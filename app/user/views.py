de6fe84bb04e73b7abbd46b5a7e1258b51d1a35d
sh.minaee@gmail.com <sh.minaee@gmail.com>
2020-09-09 20:17:07 +0430

    Add manage user endpoint Error.

--- app/user/views.py
+++ app/user/views.py
@@ -1,16 +1,27 @@
-from rest_framework import generics
+from rest_framework import generics, authentication, permissions
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.settings import api_settings
 
-from user.serializers import UserSerializers, AuthTokenSerializer
+from user.serializers import UserSerializer, AuthTokenSerializer
 
 
 class CreateUserView(generics.CreateAPIView):
     """Create a new user in he system"""
-    serializer_class = UserSerializers
+    serializer_class = UserSerializer
 
 
 class CreateTokenView(ObtainAuthToken):
     """Create a new auth token for user"""
     serializer_class = AuthTokenSerializer
     renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+
+
+class ManageUserView(generics.RetrieveUpdateAPIView):
+    """Manage the authetenticated user"""
+    serializer_class = UserSerializer
+    authentication_classes = (authentication.TokenAuthentication,)
+    permission_classes = (permissions.IsAuthenticated,)
+
+    def get_objects(self):
+        """Retirieve and return authentication user"""
+        return self.request.user
