19ff60b0442638ece7b281804f85aef5a3f56af1
sh.minaee@gmail.com <sh.minaee@gmail.com>
2020-09-10 18:20:55 +0430

    Added recipe feature.

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from core.models import Tag, Ingredient  # , Recipe
+from core.models import Tag, Ingredient, Recipe
 
 
 class TagSerializer(serializers.ModelSerializer):
@@ -19,3 +19,38 @@ class IngredientSerializer(serializers.ModelSerializer):
         model = Ingredient
         fields = ('id', 'name')
         read_only_fields = ('id',)
+
+
+class RecipeSerializer(serializers.ModelSerializer):
+    """Serialize a recipe"""
+    ingredients = serializers.PrimaryKeyRelatedField(
+        many=True,
+        queryset=Ingredient.objects.all()
+    )
+    tags = serializers.PrimaryKeyRelatedField(
+        many=True,
+        queryset=Tag.objects.all()
+    )
+
+    class Meta:
+        model = Recipe
+        fields = (
+            'id', 'title', 'ingredients', 'tags', 'time_minutes',
+            'price', 'link'
+        )
+        read_only_fields = ('id',)
+
+
+# class RecipeDetailSerializer(RecipeSerializer):
+#     """Serialize a recipe detail"""
+#     ingredients = IngredientSerializer(many=True, read_only=True)
+#     tags = TagSerializer(many=True, read_only=True)
+
+
+# class RecipeImageSerializer(serializers.ModelSerializer):
+#     """Serializer for uploading images to recipes"""
+
+#     class Meta:
+#         model = Recipe
+#         fields = ('id', 'image')
+#         read_only_fields = ('id',)
